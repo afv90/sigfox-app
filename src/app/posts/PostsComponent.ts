6d05a178af929146f66977674f7eb63a6b11e15d
Álvaro Vaquero <alvaro90.fv@gmail.com>
2019-03-18 21:28:37 +0100

    Visualizacion datos dispositivo(coordenadas, fecha mensaje, deviceID, ID)

--- src/app/posts/PostsComponent.ts
+++ src/app/posts/PostsComponent.ts
@@ -9,10 +9,21 @@ import { HttpClient } from '@angular/common/http';
 export class PostsComponent implements OnInit {
   
   posts: Object;
+  id: number;
+  DeviceID: String;
+  Latitude: number;
+  Longitude: number;
+  Date: String;
+
   
   constructor(http: HttpClient) {
     http.get('/sigfox/sigfox?deviceID=0F2223').subscribe(response => {
       this.posts = response;
+      this.id = this.posts[0].id;
+      this.DeviceID = this.posts[0].deviceId;
+      this.Latitude = this.posts[0].geoloc.latitude;
+      this.Longitude = this.posts[0].geoloc.longitude;
+      this.Date = this.posts[0].time;
     });
   }
   ngOnInit() {
